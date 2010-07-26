3a3b66833539cfabe5ed366272486e31139f84a0
ecrespo <ecrespo@localhost>
2010-07-25 21:21:02 -0530

    Agregado el uso de argparse para capturar los argumentos del comando

--- configGconf.py
+++ configGconf.py
@@ -1,114 +1,108 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 """
 Name: configGconf
 Description: Aplicación y módulo que permite modificar los accesos rápido de teclas a programas
-Version:0.2
+Version:0.3
 License: GPLv3
-Copyright: Copyright (C) 2009  Libre Accesibilidad
+Copyright: Copyright (C) 2009  Distrito Socialista Tecnologico AIT PDVSA Mérida
 Author: Ernesto Nadir Crespo Avila
 Email: ecrespo@gmail.com
 Changelog:
- * Agregada multiples opciones de configuración
-
+ 0.1: * Agregada multiples opciones de configuración.
+ 0.2: * Agregada opción de selección de distribución debian, ubuntu o canaima.
+      * Agregada información adicional a la hora de desplegar en pantalla.
+ 0.3: * Agregado el uso del módulo argparse para simplificar la captura de argumentos del comando.
 """
+__version = "0.3"
+__autor = "Ernesto Nadir Crespo Avila"
+__email = "ecrespo@gmail.com"
+__copyright = "GPLv3"
 
+#Importar módulo gconf
 import gconf
 
 
 class Conf:
     def __init__(self):
+        #Se crea la instancia de la clase de gconf
         self.gconfClient = gconf.client_get_default()
+        #Se crea la tupĺa aplicaciones        
         self.aplicaciones = ("orca", "gnome-terminal","oowriter","iceweasel","nautilus","ooimpress","pidgin","oocalc","gedit","gnome-calculator","rhythmbox")
+        #Se crea la ruta del comando del teclado y la ruta del modificador de teclado
         self.comando = "/apps/metacity/keybinding_commands/command_"
         self.asignacion_teclado = "/apps/metacity/global_keybindings/run_command_"
+        #Se crea un diccionario teclas con la asociación entre la aplicación y su acceso rápido de teclado
         self.teclas = {"orca":"<Super>o","gnome-terminal":"<Super>t","oowriter":"<Super>w","iceweasel":"<Super>n","nautilus":"<Super>h","ooimpress":"<Super>i","pidgin":"<Super>p","oocalc":"<Super>x","gedit":"<Super>e","gnome-calculator":"<Super>c","rhythmbox":"<Super>m"}
-        self.colorblind = {}
+
         
 
         
-    def modificar(self):
+    def modificar(self,distribucion):
+        """
+        modificar: Permite modificar los accesos rápidos del teclado
+        Argumentos:
+         * distribucion: se le pasa una distribución a utilizar entre debian,ubuntu y canaima.
+        """
         cont = 1
+        print "Listar accesos rapidos del teclado de gconf"
+        print "________________________________________________"
+        #Se la distribución no es debian se cambia iceweasel por firefox en las variables aplicaciones y teclas.
+        if args.distribucion <> "debian":
+            self.aplicaciones = ("orca", "gnome-terminal","oowriter","firefox","nautilus","ooimpress","pidgin","oocalc","gedit","gnome-calculator","rhythmbox")
+            self.teclas = {"orca":"<Super>o","gnome-terminal":"<Super>t","oowriter":"<Super>w","firefox":"<Super>n","nautilus":"<Super>h","ooimpress":"<Super>i","pidgin":"<Super>p","oocalc":"<Super>x","gedit":"<Super>e","gnome-calculator":"<Super>c","rhythmbox":"<Super>m"}
+        #Se genera un ciclo según las aplicaciones
         for aplicacion in self.aplicaciones:
+            #Definición de las rutas del gconf del comando y del modificador del teclado
             ruta1 =  "%s%s" %(self.comando,cont)
             ruta2 = "%s%s"  %(self.asignacion_teclado,cont)
+            #Se modifica gconf
             self.gconfClient.set_string(ruta1, "%s" %aplicacion)
             self.gconfClient.set_string(ruta2, "%s" %self.teclas[aplicacion])
+            #Se imprime en pantalla los cambios logrados.
+            print "Configurando aplicacion: %s, acceso teclado: %s" %(aplicacion,self.teclas[aplicacion])
             cont = cont +1
     
-    def modificar_opcion(self,opciones_validas):
-	cont = 1
-	for aplicacion in opciones_validas:
-	    if aplicacion in self.aplicaciones:
-		ruta1 = "%s%s" %(self.comando,cont)
-		ruta2 = "%s%s" %(self.asignacion_teclado,cont)
-		self.gconfClient.set_string(ruta1, "%s" %aplicacion)
-		self.gconfClient.set_string(ruta2, "%s" %self.teclas[aplicacion])
-		cont = cont +1
-	    else:
-		print "please change this option: %s" %aplicacion
     
     def listar(self):
+        """
+        listar: Permite listar los accesos rápidos de teclado de gconf
+        """
         cont = 1
+        print "Listar accesos rapidos del teclado a gconf"
+        print "________________________________________________"
+        #Se crea un ciclo según la lista de aplicaciones
         for aplicacion in self.aplicaciones:
+            #Se define la ruta del comando del teclado según gconf
             ruta1 =  "%s%s" %(self.comando,cont)
+            #Se define la ruta del modificador del teclado
             ruta2 = "%s%s"  %(self.asignacion_teclado,cont)
-            print self.gconfClient.get_string(ruta1),self.gconfClient.get_string(ruta2)
+            #Se desplega en pantalla 
+            print "Aplicación: " ,self.gconfClient.get_string(ruta1),self.gconfClient.get_string(ruta2)
             cont = cont +1
 
 
 if __name__ == "__main__":
-    import sys
+    #Importar módulo argparse para capturar los argumentos del comando    
+    import argparse
+    #Se instancia la clase Conf en el objeto config
     config = Conf()
+    #Se define como global a la variable opciones
     global opciones
-    opciones = config.teclas.keys()
-    def mensaje():
-        print "pyconfig-orca options " 
-        print "option : --help    : Print this help"
-        print "option : --list    : List gconf for gnome-orca"
-        print "option : --all     : add all config access key in gconf"
-        for opcion in opciones:
-			print "option : --%s   : add gconf access key to %s" %(opcion,opcion)
-    
-    if len(sys.argv) == 1 :
-        mensaje()
-    elif len(sys.argv) == 2:
-        if sys.argv[1] == "--list" :
-            config.listar()
-        elif sys.argv[1] == "--all":
-            config.modificar()
-        elif sys.argv[1] == "--help" :
-            mensaje()
-        else:
-	    for opcion in opciones:
-		if sys.argv[1] =="--%s" %opcion:
-		    config.modificar_opcion(opcion)
-    elif len(sys.argv) > 2:
-	bandera = 0
-	opciones_validas = []
-	for opcion in sys.argv[1:]:
-	    if opcion == "--list" :
-		bandera = 1
-		continue
-	    elif opcion == "--all":
-		bandera = 1
-		continue
-	    elif opcion == "--help":
-		bandera == 1
-		continue
-	    else:
-		opciones_validas.append(opcion[2:])
-	if bandera == 1:
-	    mensaje()
-	    print "If you have a two option or more, please don\'t use --list,--all or --help."
-	else:
-	    config.modificar_opcion(opciones_validas)
-	    print "all change do it"
-        
-        
-
-
-    
-    
+    #opciones contiene las claves del diccionario teclas
+    opciones = config.teclas.keys()    
+    #Creación del parse 
+    parser = argparse.ArgumentParser(prog='configGconf',description="Cambiar accesos rapidos en teclado a Gnome")
+    #Se agrega las opciones accion, distribucion y version.    
+    parser.add_argument('-a','--accion',type=str,choices=["cambiar","listar"],default=["cambiar","listar"],help='lista gconf')
+    parser.add_argument('-d','--distribucion',choices=["debian","ubuntu","canaima"],type=str,default=["debian","ubuntu","canaima"],help='seleccione entre Canaima,Debian y ubuntu')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.3')
+    #Se captura los argumentos del comando ejecutado.
+    args = parser.parse_args()
+    # ejecución de las opciones según la acción y la distribución
+    if args.accion == "listar":
+        #Se lista las configuraciones de los accesos del teclado
+        config.listar()
+    elif args.distribucion in ('debian','ubuntu','canaima') and args.accion == "cambiar":
+        #Se modifican las opciones de los accesos del teclado
+        config.modificar(args.distribucion)
